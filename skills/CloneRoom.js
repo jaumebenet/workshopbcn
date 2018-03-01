b112a9a397bc35fad30596a1f6b5528c9fef3697
Jaume <jaume.benet@es.scc.com>
2018-03-01 15:54:15 +0100

    Fase4

--- skills/CloneRoom.js
+++ skills/CloneRoom.js
@@ -6,20 +6,20 @@ controller.hears(["espacio"], 'direct_message,direct_mention', function (bot, me
             var spark = new CiscoSpark(process.env.SPARK_TOKEN);
     
             bot.startConversation(message, function (err, convo) {
-                var question = "What name do you want for the new room?";
+                var question = "Qué nombre quieres darle al nuevo espacio?";
                 convo.ask(question, [
                   {
                         default: true,
                         callback: function (response, convo) {
                             var membershipParameters = {"roomId": message.channel };
                             var roomName = response.text;
-                            convo.say("Creating room " + "**"+response.text +"**...");
+                            convo.say("Creando el espacio " + "**"+response.text +"**...");
                         async.waterfall([
                                function (callback){
                                   spark.rooms.create({"title": roomName}, function(err, response) {
                                       if(err)
                                       {
-                                          convo.say("I am sorry, there was an error creating the room, Please contact administrator.");
+                                          convo.say("Lo siento, ha habido un error creando el espacio, por favor contacta con el administrador.");
                                           convo.next(); 
                                         }
                                       else{
@@ -32,7 +32,7 @@ controller.hears(["espacio"], 'direct_message,direct_mention', function (bot, me
                                     spark.memberships.list(membershipParameters, function(err, response) {
                                         if(err)
                                         {
-                                          convo.say("I am sorry, there was an error creating the room, Please contact administrator.");
+                                          convo.say("Lo siento, ha habido un error creando el espacio, por favor contacta con el administrador.");
                                           convo.next();
                                          }
                                       else{
@@ -48,11 +48,11 @@ controller.hears(["espacio"], 'direct_message,direct_mention', function (bot, me
                                      spark.memberships.create(param, function(err, response){
                                         console.log(err);   });
                                     });
-                                    convo.say("I have created the room. You can check it now.");
+                                    convo.say("Espacio creado, ya puedes comprobarlo.");
                                     convo.next();
                                  }
                                  else{
-                                    convo.say("I am sorry, there was an error creating the room, Please contact administrator.");
+                                    convo.say("Lo siento, ha habido un error creando el espacio, por favor contacta con el administrador.");
                                     convo.next();
                                   }
                                 }
